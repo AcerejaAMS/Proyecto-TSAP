ee7ea700937b4fad5db6aa28c2490500ef154c81
AcerejaAMS <amoraess1412@gmail.com>
2025-05-04 11:00:42 -0600

    perf: Cambios en el modelo de ResNet50 y CNN perf: Cambio en el manejo de las imagenes de entrada test: Se realizaron multiples pruebas

--- resnet50.py
+++ resnet50.py
@@ -4,27 +4,17 @@ from tensorflow.keras.applications import ResNet50
 from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 import numpy as np
 
 class ResNet50_rgb:
     def __init__(self, images, labels):
-        # Reducitos tamaño para poder optimizar el uso de memoria
-        images = tf.image.resize(images, [128, 128])
-        images = tf.image.resize(images, [64, 64])
-        images = tf.image.resize(images, [32, 32])
-
-        # Normalizamos las imágenes al rango [0, 1]
-        self.X_scaled = np.array(images / 255.0)
-        self.y_onehot = np.array(to_categorical(labels))
+        # División de los datos de entrenamiento y prueba
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
 
     def CrearModelo(self):
-        # División de los datos de entrenamiento y prueba
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_scaled, self.y_onehot, test_size=0.2, random_state=42)
-
         # Cargar la base de ResNet50 sin las capas superiores
-        base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(shape=(32, 32, 3)))
+        base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(shape=(128, 128, 3)))
 
         # Congelamos las capas de ResNet50 para que no se entrenen
         for layer in base_model.layers:
@@ -34,9 +24,9 @@ class ResNet50_rgb:
         x = base_model.output
         x = GlobalAveragePooling2D()(x)
         x = Dense(256, activation='relu')(x)
-        x = Dropout(0.5)(x)
+        x = Dropout(0.3)(x)
         x = Dense(64, activation='relu')(x)
-        x = Dropout(0.5)(x)
+        x = Dropout(0.3)(x)
         predictions = Dense(9, activation='softmax')(x)
 
         # Creamos el modelo completo
@@ -50,7 +40,7 @@ class ResNet50_rgb:
         self.model.compile(optimizer=self.optimi, loss='categorical_crossentropy', metrics=['accuracy'])
 
         # Entrenamiento
-        self.history = self.model.fit(self.X_train, self.y_train, epochs=100, batch_size=16,
+        self.history = self.model.fit(self.X_train, self.y_train, epochs=100, batch_size=32,
                                     validation_split=0.1, callbacks=[self.best_model])
 
         # Carga del mejor modelo
@@ -67,22 +57,12 @@ class ResNet50_rgb:
 
 class ResNet50_gray:
     def __init__(self, images, labels):
-        # Reducitos tamaño para poder optimizar el uso de memoria
-        images = tf.image.resize(images, [128, 128])
-        images = tf.image.resize(images, [64, 64])
-        images = tf.image.resize(images, [32, 32])
-        
-        # Normalizamos las imágenes al rango [0, 1]
-        self.X_scaled = np.array(images / 255.0)
-        self.X_scaled = np.repeat(self.X_scaled, 3, axis=-1)
-        self.y_onehot = np.array(to_categorical(labels))
+        # División de los datos de entrenamiento y prueba
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(images, labels, test_size=0.2, random_state=42)
 
     def CrearModelo(self):
-        # División de los datos de entrenamiento y prueba
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X_scaled, self.y_onehot, test_size=0.2, random_state=42)
-
         # Cargar la base de ResNet50 sin las capas superiores
-        base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(shape=(32, 32, 3)))
+        base_model = ResNet50(weights='imagenet', include_top=False, input_tensor=Input(shape=(128, 128, 3)))
 
         # Congelamos las capas de ResNet50 para que no se entrenen
         for layer in base_model.layers:
@@ -92,9 +72,9 @@ class ResNet50_gray:
         x = base_model.output
         x = GlobalAveragePooling2D()(x)
         x = Dense(256, activation='relu')(x)
-        x = Dropout(0.5)(x)
+        x = Dropout(0.3)(x)
         x = Dense(64, activation='relu')(x)
-        x = Dropout(0.5)(x)
+        x = Dropout(0.3)(x)
         predictions = Dense(9, activation='softmax')(x)
 
         # Creamos el modelo completo
@@ -108,7 +88,7 @@ class ResNet50_gray:
         self.model.compile(optimizer=self.optimi, loss='categorical_crossentropy', metrics=['accuracy'])
 
         # Entrenamiento
-        self.history = self.model.fit(self.X_train, self.y_train, epochs=100, batch_size=16,
+        self.history = self.model.fit(self.X_train, self.y_train, epochs=100, batch_size=32,
                                     validation_split=0.1, callbacks=[self.best_model])
 
         # Carga del mejor modelo
